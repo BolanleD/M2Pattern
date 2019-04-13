e306f6afff5d87c63df1db2eb8a14e8e6ce6325c
BolanleD <bolafago2@yahoo.com>
2019-04-12 20:07:07 -0500

    added procfile for heroku launch and mondodb cloud

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/m2patterndb',
+  database: 'mongodb+srv://Bdahunsi1:tigercub1@cluster0-puxm0.azure.mongodb.net/m2pattern?retryWrites=true',
   secret: 'patternsecret',
 }
